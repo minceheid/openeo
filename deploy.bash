48fab70ba394d30704f4973fb186a3fb6b206e94
Mike Scott <44728295+minceheid@users.noreply.github.com>
2025-07-19 15:13:49 +0100

    Update deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -2,6 +2,6 @@
 
 echo "@reboot openeo/boot.bash" >/tmp/crontab 
 crontab /tmp/crontab
-sudo apt-get install -y python3-serial python3-websockets python3-jsonschema
+sudo apt-get install -y python3-serial python3-websockets python3-jsonschema python3-jinja2
 sudo cp /boot/firmware/config.txt /tmp/config.txt
 sudo sh -c 'sed "s/#dtparam=spi=on/dtparam=spi=on/" </tmp/config.txt >/boot/firmware/config.txt'
