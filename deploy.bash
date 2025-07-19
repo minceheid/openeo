d35ec78d0b7fb16271e2331f0d3a6185005df32c
Mike Scott <44728295+minceheid@users.noreply.github.com>
2025-07-19 15:37:25 +0100

    Update deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -1,7 +1,20 @@
 #!/usr/bin/bash
 
+# Get and extract main archive
+BRANCH=main
+
+wget https://github.com/minceheid/openeo/archive/refs/heads/$BRANCH.zip
+unzip main.zip
+# The zip file includes the branch name, so remove that for a consistent pathname
+mv openeo-$BRANCH openeo
+
+# The crontab will allow openeo to start automatically at boot
 echo "@reboot openeo/boot.bash" >/tmp/crontab 
 crontab /tmp/crontab
+
+# Install prereq packages
 sudo apt-get install -y python3-serial python3-websockets python3-jsonschema python3-jinja2
+
+# Update the SPI config
 sudo cp /boot/firmware/config.txt /tmp/config.txt
 sudo sh -c 'sed "s/#dtparam=spi=on/dtparam=spi=on/" </tmp/config.txt >/boot/firmware/config.txt'
