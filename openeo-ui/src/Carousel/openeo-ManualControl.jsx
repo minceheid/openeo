a2c99b7604f0ccc5d0077f828c568f13f6c5aad9
Mike Scott <44728295+minceheid@users.noreply.github.com>
2026-05-03 16:40:42 +0100

    Chart (#167)

---
+++ openeo-ui/src/Carousel/openeo-ManualControl.jsx
@@ -0,0 +1,65 @@
+import React, { useMemo, useRef, useState, useEffect } from "react";
+import AmpSlider from "./openeo-AmpSlider";
+import Toggle from "./openeo-Toggle";
+import HelpModal from "../openeo-HelpModal";
+
+export default function ManualControl({ schedule, onChange, onCommit,setTimersActive,active }) {
+
+  return (
+<div className="flex flex-col items-center p-5 gap-3 w-fit justify-center">
+
+
+  <HelpModal title="Manual Control">
+    <p>Manual Control lets you directly manage EV charging behaviour.</p>
+    <p><b>Timers Enabled/Disabled</b> allows or prevents scheduled charging periods to run automatically.</p>
+    <p><b>Charging Enabled/Disabled</b> When set to "Enabled", this will immediately enable charging, regardless of any timed schedules defined.</p>
+    <p>Use the <b>amp slider</b> to control the maximum charging current when manually charging.</p>
+  </HelpModal>
+  <div className="absolute top-0  items-center justify-center text-white/80 text-3xl font-semibold unselectable mt-5">
+    Manual Control
+  </div>
+
+  <Toggle
+    enabled={schedule.scheduler_enabled}
+    onChange={(v) => {
+      if(active) {
+        schedule.scheduler_enabled=v;
+        onChange({ ...schedule, scheduler_enabled: v });
+        setTimersActive(v)
+        onCommit();
+      }
+    }}
+  />
+  <div className="text-white/80 mb-2">
+    Timers {schedule.scheduler_enabled ? "Enabled" : "Disabled"}
+  </div>
+  
+  <Toggle
+    enabled={schedule.enabled}
+    onChange={(v) => {
+      if(active) {
+        schedule.enabled=v;
+        onChange({ ...schedule, enabled: v });
+        onCommit();
+      }
+    }}
+  />
+
+  <div className="text-white/80 mb-3" > 
+    Charging {schedule.enabled ? "Enabled" : "Disabled"}
+  </div>
+
+     <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[80%] mb-[5px]">
+    <AmpSlider
+      value={schedule.amps}
+      min={6}
+      max={32}
+      onChange={(v) => { active &&  onChange({ ...schedule, amps: v })} }
+      onCommit={active && onCommit}
+      active={active}
+    />
+  </div>
+</div>
+
+  );
+}
